46898e0d542cdf8b3d2b372fedb7079249554325
Galaxydragonx0 <48951583+Galaxydragonx0@users.noreply.github.com>
2021-02-15 20:25:27 -0400

    THE THING WORKING

--- encryption/encrypt.js
+++ encryption/encrypt.js
@@ -1,12 +1,16 @@
 var forge = require('node-forge');
 const fs = require('fs');
-var pubkey = fs.readFileSync(path("public_key.pem"), 'utf8')
+var pubkey = fs.readFileSync("publickey.pem", 'utf8')
 
 var publicKey = forge.pki.publicKeyFromPem(pubkey);
 
 var secretMessage = "Hello World";
-var encrypted = publicKey.encrypt(secretMessage, "RSA-OAEP", {
-            md: forge.md.sha256.create(),
-            mgf1: forge.mgf1.create()
-        });
+var encrypted = publicKey.encrypt(secretMessage, "RSA-OAEP");
 var base64 = forge.util.encode64(encrypted);
+
+// , {
+//     md: forge.md.sha256.create(),
+//     mgf1: forge.mgf1.create()
+// }
+
+console.log(encrypted);
