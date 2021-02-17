e13cd1a4f5d98be1594ff38c08f5597e122d7d8b
Galaxydragonx0 <48951583+Galaxydragonx0@users.noreply.github.com>
2021-02-17 17:14:01 -0400

    Revert "JSBN DOES NOT WORK"
    This reverts commit 8df11dd189413f09fd287b183f68538d3535ad02.

--- encryption/encrypt.js
+++ encryption/encrypt.js
@@ -1,32 +1,28 @@
-var jsbn = require('jsbn');
+var forge = require('node-forge');
+const fs = require('fs');
+var pubkey = fs.readFileSync("publickey.pem", 'utf8')
 
-var key = jsbn-rsa.RSAKey();
-console.log(key)
-
-// const fs = require('fs');
-// var pubkey = fs.readFileSync("publickey.pem", 'utf8')
-
-// var publicKey = forge.pki.publicKeyFromPem(pubkey);
+var publicKey = forge.pki.publicKeyFromPem(pubkey);
 
 
-// var secretMessage = "Hello World";
-// secretMessage = forge.util.encodeUtf8(secretMessage);
+var secretMessage = "Hello World";
+secretMessage = forge.util.encodeUtf8(secretMessage);
 
-// var encrypted = publicKey.encrypt(secretMessage, "RSA-OAEP");
+var encrypted = publicKey.encrypt(secretMessage, "RSA-OAEP");
 
-// console.log(encrypted)
+console.log(encrypted)
 
-// var base64 = forge.util.encode64(encrypted);
+var base64 = forge.util.encode64(encrypted);
 
-// var encryptedMessage = base64;
+var encryptedMessage = base64;
 
 
 
-// // console.log(decrypt);
+// console.log(decrypt);
 
-// // , {
-// //     md: forge.md.sha256.create(),
-// //     mgf1: forge.mgf1.create()
+// , {
+//     md: forge.md.sha256.create(),
+//     mgf1: forge.mgf1.create()
 // }
 
 fs.writeFileSync('message.txt', encryptedMessage);
