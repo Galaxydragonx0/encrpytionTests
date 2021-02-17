8df11dd189413f09fd287b183f68538d3535ad02
Galaxydragonx0 <48951583+Galaxydragonx0@users.noreply.github.com>
2021-02-17 17:13:51 -0400

    JSBN DOES NOT WORK

--- encryption/encrypt.js
+++ encryption/encrypt.js
@@ -1,28 +1,32 @@
-var forge = require('node-forge');
-const fs = require('fs');
-var pubkey = fs.readFileSync("publickey.pem", 'utf8')
+var jsbn = require('jsbn');
 
-var publicKey = forge.pki.publicKeyFromPem(pubkey);
+var key = jsbn-rsa.RSAKey();
+console.log(key)
 
+// const fs = require('fs');
+// var pubkey = fs.readFileSync("publickey.pem", 'utf8')
 
-var secretMessage = "Hello World";
-secretMessage = forge.util.encodeUtf8(secretMessage);
-
-var encrypted = publicKey.encrypt(secretMessage, "RSA-OAEP");
-
-console.log(encrypted)
-
-var base64 = forge.util.encode64(encrypted);
-
-var encryptedMessage = base64;
+// var publicKey = forge.pki.publicKeyFromPem(pubkey);
 
 
+// var secretMessage = "Hello World";
+// secretMessage = forge.util.encodeUtf8(secretMessage);
 
-// console.log(decrypt);
+// var encrypted = publicKey.encrypt(secretMessage, "RSA-OAEP");
 
-// , {
-//     md: forge.md.sha256.create(),
-//     mgf1: forge.mgf1.create()
+// console.log(encrypted)
+
+// var base64 = forge.util.encode64(encrypted);
+
+// var encryptedMessage = base64;
+
+
+
+// // console.log(decrypt);
+
+// // , {
+// //     md: forge.md.sha256.create(),
+// //     mgf1: forge.mgf1.create()
 // }
 
 fs.writeFileSync('message.txt', encryptedMessage);
